c1a1ccdd52bbb0739da362a6f1ced8c414c0f074
prashant kurve <kurveprashant60@gmail.com>
2025-07-22 17:26:12 +0530

    google pay

--- src/pages/StripeContainer.jsx
+++ src/pages/StripeContainer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./CheckoutForm";
+import AppleGooglePayButton from "./AppleGooglePayButton";
 
 const stripePromise = loadStripe("pk_test_51MdqYtEarL95InZuQJBw9LcLgwk22IJCzta2y1iNziXB0lZgoIDnCGcO2KUlJ5qwy6lSUlhJBxJ3aeCUDY9aqpK600vehzInnq");
 
@@ -9,7 +10,9 @@ const StripeContainer = () => {
   return (
     <Elements stripe={stripePromise}>
       <div className="w-[400px]">
-        <CheckoutForm />
+        {/* <CheckoutForm /> */}
+        <hr />
+        <AppleGooglePayButton/>
       </div>
     </Elements>
   );
